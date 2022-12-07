6b4a314087d61a6eabbab3be90e6c920f86567f1
KirillNaRabote <kirillznaet@mail.ru>
2022-12-08 00:08:30 +0300

    добавил верстку слайда

--- lab4/my-app/src/App.tsx
+++ lab4/my-app/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import styles from "./components/Slide/Slide.module.css"
 import {Slide} from './components/Slide/Slide'
 import {ToolsPanal} from './components/ToolsPanal/ToolsPanal';
 import {PresentationMaker, SlideType} from './types';
@@ -13,18 +14,18 @@ function App(props: AppProps) {
     const slides: SlideType[] = props.presentationMaker.presentation.slides
 
     const idsSelectedSlides = props.presentationMaker.idsSelectedSlides;
-    const idCurrSlide: string = idsSelectedSlides[idsSelectedSlides.length - 1]
+    const idCurrSlide: string = idsSelectedSlides[0]
 
     return (
-        <div>
+        <div className="app">
             <Navigation presentationMaker={props.presentationMaker}/>
             <ToolsPanal/>
-            <div>
+            <div className={styles.workZone}>
                 {slides.map(slide => {
                     if (slide.id === idCurrSlide) {
-                        return <Slide 
-                            key={slide.id} 
-                            slide={slide} 
+                        return <Slide
+                            key={slide.id}
+                            slide={slide}
                             idsSelectedSlides={props.presentationMaker.idsSelectedSlides}
                             idsSelectedBlocks={props.presentationMaker.idsSelectedBlocks}
                         />
