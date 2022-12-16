1cbd449b2801f1a20e033ae40c441871ae4320f3
KirillNaRabote <kirillznaet@mail.ru>
2022-12-16 04:53:38 +0300

    добавил горячие клавиши для выделения навигации

--- lab4/my-app/src/shortcuts.ts
+++ lab4/my-app/src/shortcuts.ts
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useLayoutEffect, useRef } from 'react';
 import {dispatch, returnCancel, rollBack} from "./state";
+import {selectSlides} from "./actions/navigation/navigation";
 
 const useKeyPress = (/*keys: string[], callback: Function, */node: any = null) => {
     // implement the callback ref pattern
@@ -38,6 +39,39 @@ const useKeyPress = (/*keys: string[], callback: Function, */node: any = null) =
     }, [handleKeyPress, node]);
 };
 
+const useMousePress = (/*keys: string[], callback: Function, */payload: string = "", node: any = null) => {
+    // implement the callback ref pattern
+    /*const callbackRef = useRef(callback);
+    useLayoutEffect(() => {
+        callbackRef.current = callback;
+    });*/
+
+    // handle what happens on key press
+    const handleKeyPress = useCallback(
+        (event: MouseEvent) => {
+            // check if one of the key is part of the ones we want
+            if (event.button === 0 && event.ctrlKey) {
+                dispatch(selectSlides, payload)
+            }
+        },
+        []
+    );
+
+    useEffect(() => {
+        // target is either the provided node or the document
+        const targetNode = node ?? document;
+        // attach the event listener
+        targetNode &&
+        targetNode.addEventListener("mousedown", handleKeyPress);
+
+        // remove the event listener
+        return () =>
+            targetNode &&
+            targetNode.removeEventListener("mousedown", handleKeyPress);
+    }, [handleKeyPress, node]);
+};
+
 export {
     useKeyPress,
+    useMousePress,
 }
